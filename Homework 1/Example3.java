a46d19473aa3eb0b29d45f28e75165a9d302e8dc
williamryan <williampryan4@gmail.com>
2026-01-20 13:03:42 +0000

    First commit

---
+++ Homework 1/Example3.java
@@ -0,0 +1,19 @@
+/*
+    [Java upprifjun] Skrifið Java forritið TLeikur.java fyrir eftirfarandi teningaleik: kastað er
+    tveimur 6-hliða teningum og á meðan útkoman (þ.e. summa gilda teninganna) sem
+    kemur upp fer lækkandi þá eru útkomurnar lagðar saman. Forritið hættir keyrslu um
+    leið og nýjasta útkoman er hærra en sú síðasta. Til dæmis, ef fyrstu köstin gefa 9, næstu
+    gefa 5, þriðju gefa aftur 5, og þau fjórðu 8, þá er prentað út 19 (því 9+5+5 = 19). Athugið
+    að fyrsta útkoman er alltaf með í summunni, en sú síðasta er aldrei með (hér 8). Skilið
+    kóðanum og skjáskoti af keyrslu sem sýnir útkomu allra kastanna og heildarsummuna.
+    Vísbending: Aðferðin StdRandom.uniformInt(N) býr til slembiheiltölu á bilinu [0, N−1].
+*/
+
+import edu.princeton.cs.algs4.*;
+
+public class Sexur {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        // Hér kemur ykkar kóði ...
+    }
+}
---
+++ Homework 1/Example4.java
@@ -0,0 +1,20 @@
+/*
+    [Hlaðar] (sbr. dæmi 1.3.3 í bók og æfing 3 í fyrirlestri 2) Notendaforrit framkvæmir
+    blöndu af push og pop-aðgerðum. push-aðgerðin setur heiltölurnar 0, 1, ..., 9 í þessari
+    röð á hlaðann, en pop-aðgerðirnar prenta út skilagildið. Sýnið hvenær pop-aðgerðirnar
+    koma í röð aðgerða í eftirfarandi úttaksrunum (setjið - inn í inntaksrununa þar sem
+    pop-aðgerð kemur). Fyrir þær úttaksrunur sem ekki eru mögulegar, útskýrið hvers
+    vegna:
+            a. 3 2 4 6 7 5 1 9 8 0
+            b. 1 2 4 0 3 5 6 8 9 7
+            c. 1 2 0 4 5 3 7 8 6 9
+*/
+
+import edu.princeton.cs.algs4.*;
+
+public class Sexur {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        // Hér kemur ykkar kóði ...
+    }
+}
